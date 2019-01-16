2d1bb35d6f7475d40edbfd776b41f1d435df338d
Tom curnock <curnock@gmail.com>
2019-01-15 23:28:07 -0500

    docstring and type annotations

---
+++ mypy_test.py
@@ -0,0 +1,14 @@
+from typing import Sequence
+
+def sadd(s: str) -> str:
+    t : Sequence[int] = (1, 2)
+    print(s + s)
+    return "v"
+
+
+
+if __name__ == "__main__":
+    sadd("1")
+
+    for i in range(5):
+        print(i)
--- testnumpy.py
+++ testnumpy.py
@@ -2,6 +2,9 @@ import numpy as np
 import itertools as it
 from pprint import pprint
 from scipy.special import comb
+from typing import List, Callable, Union
+
+
 
 def slice_plane(arr, axes, inds):
     if len(axes) != len(inds):
@@ -40,33 +43,52 @@ def lm(s, d):
 
 
 
-def get_diagonals():  
+def get_diagonals() -> Callable[[numpy.ndarray], List[numpy.ndarray]]:
     """ Returns a function that calculates the diagonals of an array. 
     The returned function has the following structure:
 
     Parameters
     ----------
-    arr : a numpy ndarray
+    arr : numpy.ndarray
+        `arr` is the array whose diagonals are to be calculated
 
     Returns
     -------
-    List of ndarray views of the diagonals of the input array.
-
-    Example usage
-    -------------
-
+    list:
+        A list of numpy.ndarray views of the diagonals of `arr`.
 
     Notes
     -----
-    Let *d* be the the number of dimension of the input array.
-    The number of corners is 2^d and hence the number of diagonals is 2^d / 2 = 2^(d-1).
+    Let d be the number of dimensions of `arr`.
+    
+    The number of corners is of `arr` is 2^d - a line has 2 corners, 
+    a square 4, a cube 8, a tesseract 16, ...
+    
+    The number of diagonals is 2^d / 2 since two connecting 
+    corners form a line. 
 
-    All diagonals exist is the same number of dimensions as the input array
+    Example
+    -------
+    >>> import numpy as np
+    >>> arr = np.arange(8).reshape(2, 2, 2)
+    >>> arr
+    array([[[0, 1],
+            [2, 3]],
+    <BLANKLINE>
+           [[4, 5],
+            [6, 7]]])
+    >>> diagonals = get_diagonals()
+    >>> diags = diagonals(arr)
+    >>> diags
+    [array([0, 7]), array([1, 6]), array([4, 3]), array([5, 2])]
+    >>> arr[0, 0, 0] = 99
+    >>> diags
+    [array([99,  7]), array([1, 6]), array([4, 3]), array([5, 2])]
     """
 
     diags = []
     
-    def diagonals(arr):
+    def diagonals(arr: numpy.ndarray) -> List[numpy.ndarray]:
         if arr.ndim == 1:
             diags.append(arr)
         else:
@@ -77,10 +99,8 @@ def get_diagonals():
     return diagonals
 
 
-
-
-def lines(arr, flatten = True):
-    lines = []
+def lines(arr: numpy.ndarray, flatten: bool = True) -> Union[List[numpy.ndarray], List[List[numpy.ndarray]]]: 
+    lines = [] # list of ndarray or list of list of ndarrays
     diagonals = get_diagonals()
 
     # loop over the numbers of dimensions of the plane in which the line exists
@@ -97,6 +117,9 @@ def lines(arr, flatten = True):
     return lines
 
 
+
+
+
 #arr[0,0,0] = 999
 l = lines(arr)
 #pprint(l)
@@ -105,3 +128,6 @@ l = lines(arr)
 
 #print(lm(size, dim))
 
+#if __name__ == "__main__":
+#    import doctest
+#    doctest.testmod()
